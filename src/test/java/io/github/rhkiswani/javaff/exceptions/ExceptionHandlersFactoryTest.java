aff860ef25bc39f5aa029f346f7b9c39b18f7a62
Mohamed Kiswani <rhkiswani@gmail.com>
2016-11-22 06:37:46 +0100

    cleanup, refactor, tests added

--- src/test/java/io/github/rhkiswani/javaff/exceptions/ExceptionHandlersFactoryTest.java
+++ src/test/java/io/github/rhkiswani/javaff/exceptions/ExceptionHandlersFactoryTest.java
@@ -47,8 +47,10 @@ public class ExceptionHandlersFactoryTest {
 
     @Test
     public void testFactory() throws Exception {
+
         assertThat(ExceptionHandlersFactory.getExceptionHandler(Throwable.class).getClass()).isEqualTo(DefaultExceptionHandler.class);
         ExceptionHandlersFactory.instance().overrideImp(Throwable.class, exceptionHandler);
+
         assertThat(ExceptionHandlersFactory.getExceptionHandler(Throwable.class).getClass()).isEqualTo(TestExceptionHandler.class);
         assertThat(ExceptionHandlersFactory.getExceptionHandler(SQLException.class).getClass()).isEqualTo(TestExceptionHandler.class);
         assertThat(ExceptionHandlersFactory.getExceptionHandler(Integer.class).getClass()).isEqualTo(DefaultExceptionHandler.class);
