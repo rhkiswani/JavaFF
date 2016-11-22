003943970db6d49aa97e4a8018f700a2241331cd
Mohamed Kiswani <rhkiswani@gmail.com>
2016-11-22 08:24:41 +0100

    fix deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,7 +3,7 @@ git checkout master
 git reset --hard
 git pull origin master
 mvn release:clean release:prepare -PperformRelease=true -B
-mvn release:preapre
+mvn release:perform
 git-flow release start "$1"
 git-flow release publish "$1"
 git-flow release finish "$1"
